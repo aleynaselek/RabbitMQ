d47da24bea702e88514ec4670dc58d5d91b04b16
aleynaflo <selekaleyna@gmail.com>
2024-08-08 00:06:34 +0300

    RabbitMQ bağlantısı

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factorty = new ConnectionFactory();
+ 
+factorty.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factorty.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "Hello World!";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "hello-queue", null, messageBody);
+
+Console.WriteLine("Mesaj gönderilmiştir.");
+
+Console.ReadLine();
+
+
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factorty = new ConnectionFactory();
+
+factorty.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factorty.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (sender, eventArgs) =>
+{
+    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj: " + message); 
+};
+ 
+
+Console.ReadLine();
+
+
