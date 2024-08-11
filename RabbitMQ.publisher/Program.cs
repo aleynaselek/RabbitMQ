d487e60c0189d1b428047420c5b5b08333f2ff0b
aleynaflo <selekaleyna@gmail.com>
2024-08-11 15:19:46 +0300

    Direct Exchange

--- RabbitMQ.publisher/Program.cs
+++ RabbitMQ.publisher/Program.cs
@@ -9,18 +9,38 @@ using var connection = factorty.CreateConnection();
 
 var channel = connection.CreateModel();
 
-channel.ExchangeDeclare("logs-fanout", ExchangeType.Fanout,true); 
+channel.ExchangeDeclare("logs-direct", ExchangeType.Direct,true); 
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+{
+    var routeKey = $"route-{x}";
+    var queueName = $"direct-queue-{x}";
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+});
 
 Enumerable.Range(1, 50).ToList().ForEach(x =>
 {
-    string message = $"log {x}";
+    LogNames log = (LogNames)new Random().Next(1, 5);
+
+    string message = $"log-type: {log}";
 
     var messageBody = Encoding.UTF8.GetBytes(message);
 
-    channel.BasicPublish("logs-fanout", "", null, messageBody);
+    var routeKey = $"route-{log}";
 
-    Console.WriteLine($"Mesaj gönderilmiştir: {message}");
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log gönderilmiştir: {message}");
 
 }); 
 
 Console.ReadLine();
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
--- RabbitMQ.subscriber/Program.cs
+++ RabbitMQ.subscriber/Program.cs
@@ -8,22 +8,25 @@ factorty.Uri = new Uri("amqp://guest:guest@localhost:5672");
 
 using var connection = factorty.CreateConnection();
 
-var channel = connection.CreateModel();
-
-var randomQueueName = channel.QueueDeclare().QueueName;
-  
-channel.QueueBind(randomQueueName, "logs-fanout", "",null);
+var channel = connection.CreateModel();  
 
 channel.BasicQos(0, 1, false);
 
 var consumer = new EventingBasicConsumer(channel);
 
-channel.BasicConsume(randomQueueName, false, consumer);
+var queueName = "direct-queue-Critical";
+
+channel.BasicConsume(queueName, false, consumer);
+
 Console.WriteLine("Loglar dinleniyor...");
+
 consumer.Received += (sender, eventArgs) =>
 {
     var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+
     Console.WriteLine("Gelen Mesaj: " + message); 
+
+    File.AppendAllText("log-critical.txt", message + Environment.NewLine);
     channel.BasicAck(eventArgs.DeliveryTag, false);
 };
  
